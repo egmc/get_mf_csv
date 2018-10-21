bcb9c4a7e6f55f397ddb8a8f936dfe89a71cbd7a
egmc <waltz4debby@eg2mix.com>
2018-10-21 18:20:08 +0900

    update if rec has changed

--- store_mf_db.php
+++ store_mf_db.php
@@ -16,8 +16,8 @@ ORM::configure('username', $db_config['mysql']['user']);
 if (isset($db_config['mysql']['pass'])) {
     ORM::configure('password', $db_config['mysql']['pass']);
 }
-//ORM::configure('error_mode', PDO::ERRMODE_EXCEPTION);
-ORM::configure('error_mode', PDO::ERRMODE_SILENT);
+ORM::configure('error_mode', PDO::ERRMODE_EXCEPTION);
+//ORM::configure('error_mode', PDO::ERRMODE_SILENT);
 
 $login_id = $mf_config['mf']['user'];
 $password = $mf_config['mf']['pass'];
@@ -25,20 +25,50 @@ $datetime = new DateTime();
 if (isset($argv[1])) {
     $datetime = new DateTime($argv[1]);
 }
+$now = new DateTime();
 
 $csv_data = shell_exec("php get_mf_csv.php {$login_id} {$password} {$datetime->format('Y-m')}");
 $csv_data = mb_convert_encoding($csv_data, 'utf8', 'sjis-win');
-//var_dump($csv_data);
 $csv_list = explode("\n", str_replace(array("\r\n", "\r", "\n"), "\n", $csv_data));
-var_dump($csv_list);
-$rows = ['calc', 'adate', 'note', 'amnt', 'serv', 'lctg', 'mctg', 'memo', 'transfer', 'mfid'];
+//var_dump($csv_list);
+$cols = ['calc', 'adate', 'note', 'amnt', 'serv', 'lctg', 'mctg', 'memo', 'transfer', 'mfid'];
 array_shift($csv_list);
-array_pop($csv_list);
 foreach ($csv_list as $row) {
+    if (!$row) {
+        continue;
+    }
     $crec = str_getcsv($row);
-    $drec = array_combine($rows, $crec);
-    $account = ORM::for_table('account_book')->create();
-    $account->set($drec);
-    $account->set(['created' => (new DateTime())->format('Y-m-d H:i:s')]);
-    $account->save();
+    if (count($crec) !== count($cols)) {
+        echo "cols count not match : {$row}";
+        continue;
+    }
+    $drec = array_combine($cols, $crec);
+
+    $orm = ORM::for_table('account_book');
+    $dobj = $orm->whereEqual('mfid', $drec['mfid'])->findOne();
+    if ($dobj) {
+        $updated = false;
+        foreach ($cols as $k) {
+            if ($k == 'adate') {
+                continue;
+            }
+            if ($dobj[$k] != $drec[$k]) {
+                echo "$k is updated\n";
+                $updated = true;
+                break;
+            }
+        }
+        if ($updated) {
+            $dobj->set($drec);
+            $dobj->set('updated', $now->format('Y-m-d H:i:s'));
+            $dobj->save();
+        }
+    } else {
+        $account = $orm->create();
+        $account->set($drec);
+        $account->set('updated', $now->format('Y-m-d H:i:s'));
+        $account->set(['created' => $now->format('Y-m-d H:i:s')]);
+        $account->save();
+    }
+
 }
